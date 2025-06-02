918b5aba789195cc5f304b62e55f190dfdb451c3
Pallavi Amit Kumar <pallavizanje@gmail.com>
2025-06-02 20:55:38 +0530

    Update request.sh

--- request.sh
+++ request.sh
@@ -1,42 +1,26 @@
-build_request_payload() {
-  local feed_def_key="$1"
-  local bussiness_date="$2"
-  local record_count="$3"
-  local feed_name="$4"
-  local fixed_upper="$5"
-  local fixed_lower="$6"
-  local window_size="$7"
-  local weekend_expected="$8"
-  local dynamic_log_data="$9"
-  local context_id="${10}"
-  local dataset_name="${11}"
-
-  jq -n \
-    --arg feed_def_key "$feed_def_key" \
-    --arg bussiness_date "$bussiness_date" \
-    --argjson record_count "$record_count" \
-    --arg feed_name "$feed_name" \
-    --argjson FixedUpperLimit "$fixed_upper" \
-    --argjson FixedLowerLimit "$fixed_lower" \
-    --argjson dynamicWindowSize "$window_size" \
-    --arg weekendFileExpected "$weekend_expected" \
-    --argjson dynamic_log_data "$dynamic_log_data" \
-    --arg context_id "$context_id" \
-    --arg dataset_name "$dataset_name" \
-    '{
-      rctConfig: {
-        type: "DYNAMIC",
-        FixedUpperLimit: $FixedUpperLimit,
-        FixedLowerLimit: $FixedLowerLimit,
-        dynamicWindowSize: $dynamicWindowSize,
-        weekendFileExpected: $weekendFileExpected,
-        feed_def_key: $feed_def_key,
-        bussiness_date: $bussiness_date,
-        record_count: $record_count,
-        feed_name: $feed_name,
-        context_id: $context_id,
-        dataset_name: $dataset_name
-      },
-      dynamic_log_data: $dynamic_log_data
-    }'
-}
+SELECT COALESCE(json_agg(json_build_object(
+  'feedDefKey', feed_def_key,
+  'feedInstKey', feed_inst_key,
+  'srcFeedDefKey', src_feed_def_key,
+  'srcFeedInstKey', src_feed_inst_key,
+  'feedName', feed_name,
+  'srcFeedName', src_feed_name,
+  'srcFeedInstName', src_feed_inst_name,
+  'businessDate', TO_CHAR(business_date, 'YYYY-MM-DD'),
+  'recordCount', record_count,
+  'iqrLowerThreshold', iqr_lower_threshold,
+  'iqrUpperThreshold', iqr_upper_threshold,
+  'zscore', zscore,
+  'gmmLowerThreshold', gmm_lower_threshold,
+  'gmmUpperThreshold', gmm_upper_threshold,
+  'iqrOutlier', COALESCE(iqr_outlier, ''),
+  'zscoreOutlier', COALESCE(zscore_outlier, ''),
+  'ifOutlier', COALESCE(if_outlier, ''),
+  'gmmOutlier', COALESCE(gmm_outlier, ''),
+  'anomalyPercentage', anomaly_percentage,
+  'lastUpdatedOn', TO_CHAR(last_updated_on, 'YYYY-MM-DD"T"HH24:MI:SS')
+)), '[]')
+FROM t_dqp_adaptive_threshold_log
+WHERE feed_def_key = '$feed_def_key'
+ORDER BY business_date DESC
+LIMIT $threshold_window;
